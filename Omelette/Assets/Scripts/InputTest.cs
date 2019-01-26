1ca14d9ad226109003e93bf5d0aa6fa6086f3f27
Donald Trump <EyseBeth@gmail.com>
2019-01-26 12:38:17 +0100

    Fixed VR in art scene. Now with working throwable

---
+++ Omelette/Assets/Scripts/InputTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class InputTest : MonoBehaviour
+{
+    private Hand hand;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Debug.Log(hand.trackedObject.)
+    }
+}
