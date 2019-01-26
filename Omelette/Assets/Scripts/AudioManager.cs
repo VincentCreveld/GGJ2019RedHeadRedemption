1b25e0fde6d1822d023b37b84978e7766da8e447
Vincent <vincentmc99@hotmail.com>
2019-01-26 15:03:45 +0100

    Blinkie made ready for test

---
+++ Omelette/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+	[SerializeField]
+	private List<AudioNameKeyPair> audioList;
+	private Dictionary<string, AudioClip> audioLibrary;
+
+	private AudioClip currentClip;
+	private Queue<AudioClip> audioQueue;
+
+	[SerializeField]
+	private AudioSource audioSource;
+	private bool isAudioPlaying;
+
+	private void Awake()
+	{
+		audioLibrary = new Dictionary<string, AudioClip>();
+		foreach (var item in audioList)
+		{
+			audioLibrary.Add(item.name, item.clip);
+		}
+
+		audioQueue = new Queue<AudioClip>();
+	}
+	private void Start()
+	{
+		StartCoroutine(AudioCycle());
+	}
+
+	private IEnumerator AudioCycle()
+	{
+		while (true)
+		{
+			while (audioQueue.Count <= 0 || isAudioPlaying)
+				yield return null;
+
+			currentClip = audioQueue.Dequeue();
+			float delay = currentClip.length;
+			audioSource.clip = currentClip;
+			audioSource.Play();
+			isAudioPlaying = true;
+			yield return new WaitForSeconds(delay);
+			Debug.Log("here");
+			audioSource.Stop();
+			audioSource.clip = null;
+			isAudioPlaying = false;
+		}
+	}
+	
+	public void PlayAudio(string audioClip)
+	{
+		if (audioLibrary.ContainsKey(audioClip))
+			audioQueue.Enqueue(audioLibrary[audioClip]);
+	}
+
+	[ContextMenu("yeet")]
+	public void PlaySound()
+	{
+		PlayAudio("yeet");
+	}
+
+	[ContextMenu("bruh")]
+	public void PlaySound2()
+	{
+		PlayAudio("bruh");
+	}
+
+	[ContextMenu("alarm")]
+	public void PlaySound3()
+	{
+		PlayAudio("alarm");
+	}
+}
+
+[Serializable]
+public class AudioNameKeyPair
+{
+	public string name;
+	public AudioClip clip;
+}
---
+++ Omelette/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LevelManager : MonoBehaviour
+{
+	public static LevelManager instance;
+
+	[SerializeField]
+	private List<NavMeshAgent> enemies;
+
+	[SerializeField]
+	private AudioManager announcementManager;
+
+	private string clipToPlay = "yeet";
+
+	public Transform bottlePos;
+
+	private void Awake()
+	{
+		if (instance == null)
+			instance = this;
+		else
+		{
+			Debug.LogError("Too many LevelManagers in scene. Disabling LevelManager on " + name);
+			this.enabled = false;
+		}
+	}
+
+	public void RegisterBottle(Vector3 pos)
+	{
+		StartCoroutine(SendAgentToPos(2f, GetClosestAgent(pos), pos));
+	}
+
+	private IEnumerator SendAgentToPos(float delay, NavMeshAgent agent, Vector3 pos)
+	{
+		yield return new WaitForSeconds(delay);
+		agent.GetComponent<Awareness>().MoveToClean(pos);
+		announcementManager.PlayAudio(clipToPlay);
+	}
+
+	private NavMeshAgent GetClosestAgent(Vector3 pos)
+	{
+		NavMeshAgent closestEnemy = null;
+		float closestDist = 100f;
+		foreach (var enemy in enemies)
+		{
+			float dist = Vector3.Distance(enemy.transform.position, pos);
+			if (dist < closestDist)
+			{
+				closestEnemy = enemy;
+				closestDist = dist;
+			}
+		}
+
+		return closestEnemy;
+	}
+
+	[ContextMenu("bottle")]
+	public void MovetoBottle()
+	{
+		RegisterBottle(bottlePos.position);
+	}
+}
