9cf5b1915574ce0ac9a2f8804a1700e6787e8725
Vincent <vincentmc99@hotmail.com>
2019-01-26 03:01:43 +0100

    Controller changes

--- Omelette/Assets/Scripts/PlayerMovement.cs
+++ Omelette/Assets/Scripts/PlayerMovement.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Valve.VR;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+	private enum InputType { viveController, mouse }
+	[SerializeField]
+	private InputType inputType;
+
 	[SerializeField]
 	private NavMeshAgent playerModel;
 	[SerializeField]
@@ -16,30 +22,49 @@ public class PlayerMovement : MonoBehaviour
 	[SerializeField]
 	private RayLineScaler raycaster;
 
+	private bool isInputDown = false, prevIsInputDown = false;
 
 	private void Update()
 	{
-		if (Input.GetKey(KeyCode.Space))
+		if (IsSelectionPressed())
 		{
+			isInputDown = true;
 			raycaster.gameObject.SetActive(true);
 			playerModel.isStopped = false;
 			playerModel.SetDestination(raycaster.GetCursorPos());
 		}
 		else
 		{
+			isInputDown = false;
 			raycaster.gameObject.SetActive(false);
 		}
 
-		if (Input.GetKeyUp(KeyCode.Space))
+		// Defines onButtonUp timing
+		if (prevIsInputDown != isInputDown && isInputDown == false)
 		{
 			TeleportCamera();
 			playerModel.isStopped = true;
 		}
+
+		prevIsInputDown = isInputDown;
 	}
 
 	// Should be a slow teleport with fade out and fade in.
 	private void TeleportCamera()
 	{
 		camObject.position = camPosInPlayer.position;
+	}
+
+	private bool IsSelectionPressed()
+	{
+		switch (inputType)
+		{
+			case InputType.mouse:
+				return Input.GetKey(KeyCode.Space);
+			case InputType.viveController:
+				return SteamVR_Input._default.inActions.Teleport.GetActive(SteamVR_Input_Sources.Any);
+			default:
+				return true;
+		}
 	}
 }
