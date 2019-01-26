e22f1f0b63e6603bc37da10e3399242bb6371921
Vincent <vincentmc99@hotmail.com>
2019-01-26 02:33:04 +0100

    SteamVR added.
    Playermovement setup made

---
+++ Omelette/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField]
+	private NavMeshAgent playerModel;
+	[SerializeField]
+	private Transform camPosInPlayer;
+
+	[SerializeField]
+	private Transform camObject;
+
+	[SerializeField]
+	private RayLineScaler raycaster;
+
+
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			raycaster.gameObject.SetActive(true);
+			playerModel.isStopped = false;
+			playerModel.SetDestination(raycaster.GetCursorPos());
+		}
+		else
+		{
+			raycaster.gameObject.SetActive(false);
+		}
+
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			TeleportCamera();
+			playerModel.isStopped = true;
+		}
+	}
+
+	// Should be a slow teleport with fade out and fade in.
+	private void TeleportCamera()
+	{
+		camObject.position = camPosInPlayer.position;
+	}
+}
---
+++ Omelette/Assets/Scripts/RayLineScaler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayLineScaler : MonoBehaviour
+{
+	[SerializeField]
+	private Transform lineToScale;
+
+	[SerializeField]
+	private Transform cursor;
+	private bool isCursorValid = false;
+
+	[SerializeField]
+	private LayerMask layerMask;
+
+	private void Update()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, transform.forward, out hit, 50f, layerMask))
+		{
+			cursor.position = hit.point;
+			float scale = 0f;
+
+			scale = Vector3.Distance(lineToScale.position, hit.point);
+
+			if (scale > 0)
+			{
+				isCursorValid = true;
+				cursor.position = hit.point;
+				lineToScale.LookAt(hit.point);
+				lineToScale.transform.localScale = new Vector3(lineToScale.transform.localScale.x, lineToScale.transform.localScale.y, scale);
+			}
+		}
+		else
+		{
+			isCursorValid = false;
+			//Fallback in case no hit is found.
+			lineToScale.LookAt(lineToScale.position + lineToScale.forward);
+			lineToScale.transform.localScale = new Vector3(lineToScale.transform.localScale.x, lineToScale.transform.localScale.y, 40f);
+		}
+	}
+
+	public bool GetIsCursorValid()
+	{
+		return isCursorValid;
+	}
+
+	public Vector3 GetCursorPos()
+	{
+		return cursor.position;
+	}
+}
