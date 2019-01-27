5314ad2f0141ec6af0e1557f8fe8351bc8fa105a
Vincent <vincentmc99@hotmail.com>
2019-01-27 05:15:02 +0100

    Experimental material blend/mask spawn

--- Omelette/Assets/Scripts/EnemyModelAssigner.cs
+++ Omelette/Assets/Scripts/EnemyModelAssigner.cs
@@ -18,6 +18,17 @@ public class EnemyModelAssigner : MonoBehaviour
 	[SerializeField]
 	private GameObject maleEmployee, maleCustomer;
 	private GameObject animatedComponent;
+	[SerializeField]
+	private GameObject mask;
+	private GameObject maskInstance;
+	private GetAnimationHead animHead;
+
+	[SerializeField]
+	private Material monsterMat;
+	private Material currentMat;
+	private Color startingColor;
+
+	private float sanity = 0f;
 
 	private void Start()
 	{
@@ -25,28 +36,54 @@ public class EnemyModelAssigner : MonoBehaviour
 			enabled = false;
 
 		if (awareness.GetIsEmployee())
+		{
 			animatedComponent = Instantiate(maleEmployee, transform);
+		}
 		else
+		{
 			animatedComponent = Instantiate(maleCustomer, transform);
+		}
 
 		anim = animatedComponent.GetComponentInChildren<Animator>();
 		animatedComponent.transform.localPosition -= (Vector3.up * 0.5f);
+		animHead = animatedComponent.GetComponentInChildren<GetAnimationHead>();
+
+		currentMat = animatedComponent.GetComponent<Renderer>().material;
+		startingColor = currentMat.color;
+
+		maskInstance = Instantiate(mask, animHead.head);
+		maskInstance.SetActive(false);
 	}
 
 	private void Update()
 	{
 		SetIsWalking();
 		SetAnimSanity();
+
+		if (sanity > 0.6f)
+			maskInstance.SetActive(true);
+
+		currentMat.color = BlendColors();
 	}
 
 	private void SetAnimSanity()
 	{
-		anim.SetFloat("Sanity", playerManager.GetCurrentSanityOnScale01());
+		sanity = playerManager.GetCurrentSanityOnScale01();
+		anim.SetFloat("Sanity", sanity);
 	}
 
 	private void SetIsWalking()
 	{
 		bool val = agent.velocity.magnitude > 1.2f;
 		anim.SetBool("IsWalking", val);
+	}
+
+	private Color BlendColors()
+	{
+		Color col = new Color();
+
+		col = Color.Lerp(startingColor, monsterMat.color, Mathf.Abs(1-sanity));
+
+		return col;
 	}
 }
