6690f93373c18ce3f17e347b2dbc6286cc0a962a
Vincent <vincentmc99@hotmail.com>
2019-01-27 14:43:14 +0100

    AudioOnImpact

---
+++ Omelette/Assets/Scripts/PlayClipOnHitGround.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayClipOnHitGround : MonoBehaviour
+{
+	private bool isPopped = false;
+
+	public AudioClip clipToPlay;
+	public AudioSource anim;
+
+	private void Awake()
+	{
+		anim.clip = clipToPlay;
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.transform.tag == "Floor" && !isPopped)
+		{
+			isPopped = true;
+			anim.Play();
+		}
+	}
+}
