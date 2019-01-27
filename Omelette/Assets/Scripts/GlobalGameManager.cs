57172c57f72f74264e7c478034947c4391179278
Donald Trump <EyseBeth@gmail.com>
2019-01-27 11:35:41 +0100

    Fixed scenes SerializeFields and added colliders to everything in kitchenScene

--- Omelette/Assets/Scripts/GlobalGameManager.cs
+++ Omelette/Assets/Scripts/GlobalGameManager.cs
@@ -6,20 +6,20 @@ using UnityEngine.SceneManagement;
 public class GlobalGameManager : MonoBehaviour
 {
     [SerializeField]
-    private Scene kitchenScene;
+    private string kitchenSceneName;
     [SerializeField]
-    private Scene[] levels;
+    private string[] levelNames;
     public bool[] levelFinished;
 
     public void FinishLevel()
     {
         levelFinished[SceneManager.GetActiveScene().buildIndex] = true;
-        SceneManager.LoadScene(kitchenScene.name);
+        SceneManager.LoadScene(kitchenSceneName);
     }
 
     public void GoToNextLevel()
     {
-        for (int i = 0; i < levels.Length; i++)
+        for (int i = 0; i < levelNames.Length; i++)
         {
             if (levelFinished[i] == false)
             {
