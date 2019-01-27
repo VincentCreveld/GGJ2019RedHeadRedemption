3497af4e0af5855f8077084d656352c894256862
Vincent <vincentmc99@hotmail.com>
2019-01-27 04:37:54 +0100

    All character animations in scene
    "Art" renamed to "MainScene"
    Some folder reorganisation

---
+++ Omelette/Assets/Scripts/EnemyModelAssigner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyModelAssigner : MonoBehaviour
+{
+	[SerializeField]
+	private Awareness awareness;
+
+	private Animator anim;
+
+	[SerializeField]
+	private NavMeshAgent agent;
+
+	[SerializeField]
+	private GameObject maleEmployee, maleCustomer;
+	private GameObject animatedComponent;
+
+	private void Start()
+	{
+		if (awareness == null)
+			enabled = false;
+
+		if (awareness.GetIsEmployee())
+			animatedComponent = Instantiate(maleEmployee, transform);
+		else
+			animatedComponent = Instantiate(maleCustomer, transform);
+
+		anim = animatedComponent.GetComponentInChildren<Animator>();
+		animatedComponent.transform.localPosition -= (Vector3.up * 0.5f);
+	}
+
+	private void Update()
+	{
+		SetIsWalking();
+	}
+
+	private void SetIsWalking()
+	{
+		bool val = agent.velocity.magnitude > 1.2f;
+		anim.SetBool("IsWalking", val);
+	}
+}
